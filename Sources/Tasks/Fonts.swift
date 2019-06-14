40b78648fbbd2af7ac82d26bba49a193584a853f
Hal Lee <hal@lee.me>
2019-06-13 21:02:50 -0400

    Recursive search for fonts + installation!

--- Sources/Tasks/Fonts.swift
+++ Sources/Tasks/Fonts.swift
@@ -3,14 +3,63 @@ import PromiseKit
 
 struct Fonts {
 
+    private static var fontPath: String {
+        FileManager.default.currentDirectoryPath + "/fonts"
+    }
+
+    private static var fontInstallPath: String {
+        FileManager.default.homeDirectoryForCurrentUser.path + "/Library/Fonts"
+    }
+
     static func install() -> Promise<Void> {
         try? removeOldFontDirectory()
-        return Git.clone("git@github.com:hallee/fonts.git")
+        return firstly {
+            Git.clone("git@github.com:hallee/fonts.git")
+        }.then {
+            recursivelyFindAllFonts()
+        }.then { urls in
+            installFonts(urls)
+        }
+    }
+
+    /// Searches for fonts based on their type identifier (UTI)
+    private static func recursivelyFindAllFonts() -> Promise<[URL]> {
+        return Promise<[URL]> { seal in
+            var fonts = [URL]()
+
+            let searcher = FileManager.default.enumerator(
+                at: URL(fileURLWithPath: fontPath),
+                includingPropertiesForKeys: [.typeIdentifierKey],
+                options: [.skipsPackageDescendants, .skipsHiddenFiles]
+            )
+
+            while let file = searcher?.nextObject() as? URL {
+                guard let identifier = try? file.resourceValues(
+                    forKeys: [.typeIdentifierKey]
+                ).typeIdentifier else { continue }
+                /// Seems reasonable that the UTI name contains "font" for fonts,
+                /// there probably are edge cases but oh well
+                guard identifier.contains("font") else { continue }
+
+                fonts.append(file)
+            }
+
+            seal.fulfill(fonts)
+        }
+    }
+
+    private static func installFonts(_ fonts: [URL]) -> Promise<Void> {
+        return Promise<Void> { seal in
+            fonts.forEach { font in
+                try? FileManager.default.moveItem(atPath: font.path,
+                                                  toPath: fontInstallPath)
+            }
+            seal.fulfill()
+        }
     }
 
     private static func removeOldFontDirectory() throws {
-        let path = FileManager.default.currentDirectoryPath + "/fonts"
-        try FileManager.default.removeItem(atPath: path)
+        try FileManager.default.removeItem(atPath: fontPath)
     }
 
 }
