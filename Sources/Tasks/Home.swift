9b80b62def992ac86e1079a64beecc2391c09fa3
Hal Lee <hal@lee.me>
2019-06-15 16:52:36 -0400

    Fixed symlinking

--- Sources/Tasks/Home.swift
+++ Sources/Tasks/Home.swift
@@ -12,11 +12,21 @@ struct Home {
             let searcher = FileManager.default.enumerator(
                 at: dotfilesDirectoryURL,
                 includingPropertiesForKeys: nil,
-                options: [.skipsPackageDescendants,
-                          .skipsSubdirectoryDescendants]
+                options: [.skipsPackageDescendants]
             )
             while let file = searcher?.nextObject() as? URL {
-                let destination = Constants.home.appendingPathComponent(file.lastPathComponent)
+                guard !file.hasDirectoryPath else { continue }
+                guard file.lastPathComponent != ".DS_Store" else { continue }
+
+                var relativeFileComponents = file.pathComponents
+                for component in dotfilesDirectoryURL.pathComponents {
+                    if let index = relativeFileComponents.firstIndex(where: {$0 == component}) {
+                        relativeFileComponents.remove(at: index)
+                    }
+                }
+                let relativeFilePathComponent = relativeFileComponents.joined(separator: "/")
+                let destination = Constants.home.appendingPathComponent(relativeFilePathComponent)
+
                 try? FileManager.default.removeItem(at: destination)
                 try FileManager.default.createSymbolicLink(
                     at: destination,
